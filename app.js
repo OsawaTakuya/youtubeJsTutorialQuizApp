00647cbf30806313dbccdf87ebc790728cd3ac09
OsawaTakuya <takuya.osawa@uni-sax.com>
2022-12-04 16:34:33 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+{
+    const question = 'たくやが好きなシチュエーションは？';
+    const answer = [
+        '拘束シチュ',
+        '逆寝取りシチュ',
+        '声を出せない状況シチュ',
+        '入院シチュ',
+    ];
+    const correct = '逆寝取りシチュ';
+
+    const element = document.getElementsByTagName('div');
+    console.log(element)
+
+
+}
