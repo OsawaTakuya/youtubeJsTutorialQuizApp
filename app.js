acf009b0e121d2f4584e18dd5585a9a42aaf3362
OsawaTakuya <takuya.osawa@uni-sax.com>
2022-12-07 00:53:24 +0900

    プロトタイプ完成、後は問題数を増やすのみだ

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 {
+    //クイズ
     const question = 'たくやが好きなシチュエーションは？';
     const answer = [
         '拘束シチュ',
@@ -7,9 +8,40 @@
         '入院シチュ',
     ];
     const correct = '逆寝取りシチュ';
+    
+    
+    const $questionArea = document.getElementById('js-question');
+    const $button = document.getElementsByClassName('btn')
+    
+    //関数群
+    const setupQuiz = ()=>{
+        $questionArea.textContent = question
+        for(let i=0;i<$button.length;i++){
+            $button[i].textContent = answer[i]
+        };
+    };
 
-    const element = document.getElementsByTagName('div');
-    console.log(element)
+    const isCorrect = (answer)=>{
+        return answer===correct?true:false;
+    };
+    const displayAnswer = (bool)=>{
+        if(bool){
+            window.alert("正解")
+        }else{
+            window.alert("不正解")
+        };
+    };
+
+
+    //メインの処理
+    setupQuiz();
+    for(let b of $button){
+        b.addEventListener(
+            'click',()=>{displayAnswer(isCorrect(b.textContent))}
+        );
+    }
+    
+
 
 
 }
